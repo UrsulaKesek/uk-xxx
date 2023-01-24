8ecacec2f3e917919594008f850ba27a90e71dcd
Ursula Kesek <ursky@blueyonder.co.uk>
2023-01-24 15:52:15 +0000

    deleted newlist and userlist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-import UserList from "./UserList";
-import NewList from "./NewList";
 import Playfield from "./MatchMatch/Playfield"
 function App() {
   return (
     <div className="App">
       <>
       <Playfield pairs={12} />
-        <UserList />
-        <NewList />
 
       </>
     </div>
